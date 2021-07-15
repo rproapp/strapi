5bd1a92cd6e198cbdb6028f8feac3b3066be2e7e
rproapp <soporte@rproapp.com>
2021-07-15 17:49:46 +0000

    strapi

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.4ikql.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'rproapp-web-db'),
+        password: env('DATABASE_PASSWORD', 'iiAXGqJSraS4eREF'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e9b7af8-0f85-4837-bffc-2ac1b6d9d215'
+};
